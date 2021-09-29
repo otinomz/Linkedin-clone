01c253af96e222375833aefed0dd32ac7fa9aeec
otinomz <awesomeotinomz@gmail.com>
2021-09-29 11:52:09 +0100

    login page layout

--- src/App.js
+++ src/App.js
@@ -3,18 +3,27 @@ import './App.css';
 import Feed from './components/Feed';
 import Header from './components/Header';
 import Sidebar from './components/Sidebar';
+import { selectUser } from './features/user/userSlice';
+import {useSelector} from 'react-redux'
+import Login from './components/Login';
 
 function App() {
+  const user= useSelector(selectUser)
   
   return (
     <div className="app">
       <Header/>
 
-      {/* App Body */}
-      <div className="app__body">
-        <Sidebar />
-        <Feed/>
-      </div>
+      { !user ? (
+          <Login />
+        ) : ( 
+          <div className="app__body">
+            <Sidebar />
+            <Feed/>
+          </div>
+         
+        )
+      }
 
     </div>
   );
---
+++ src/components/Login.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './Login.css'
+
+function Login() {
+    return (
+        <div className="login">
+            this is the login page
+        </div>
+    )
+}
+
+export default Login
